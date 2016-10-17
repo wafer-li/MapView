05153325f8a90402cccce51531129f2dd42fc7cd
Wafer Li <omyshokami@gmail.com>
2016-10-17 12:30:33 +0900

    Modify RouteLayerTestActivity for demostration

--- demo/src/main/java/com/onlylemi/mapview/RouteLayerTestActivity.java
+++ demo/src/main/java/com/onlylemi/mapview/RouteLayerTestActivity.java
@@ -3,6 +3,7 @@ package com.onlylemi.mapview;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PointF;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,29 +11,37 @@ import android.view.MenuItem;
 
 import com.onlylemi.mapview.library.MapView;
 import com.onlylemi.mapview.library.MapViewListener;
+import com.onlylemi.mapview.library.layer.LocationLayer;
 import com.onlylemi.mapview.library.layer.MarkLayer;
 import com.onlylemi.mapview.library.layer.RouteLayer;
 import com.onlylemi.mapview.library.utils.MapUtils;
 
 import java.io.IOException;
+import java.security.cert.PolicyNode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RouteLayerTestActivity extends AppCompatActivity {
 
     private MapView mapView;
 
-    private MarkLayer markLayer;
-    private RouteLayer routeLayer;
+    private MarkLayer     markLayer;
+    private RouteLayer    routeLayer;
+    private LocationLayer locationLayer;
 
-    private List<PointF> nodes;
-    private List<PointF> nodesContract;
-    private List<PointF> marks;
-    private List<String> marksName;
+    private List<PointF>  nodes;
+    private List<PointF>  nodesContract;
+    private List<PointF>  marks;
+    private List<String>  marksName;
+    private List<Integer> routeList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_layer_test);
 
@@ -53,6 +62,13 @@ public class RouteLayerTestActivity extends AppCompatActivity {
         mapView.setMapViewListener(new MapViewListener() {
             @Override
             public void onMapLoadSuccess() {
+
+                locationLayer = new LocationLayer(mapView, marks.get(39));
+                locationLayer.setOpenCompass(true);
+                locationLayer.setCompassIndicatorCircleRotateDegree(60);
+                locationLayer.setCompassIndicatorArrowRotateDegree(-30);
+                mapView.addLayer(locationLayer);
+
                 routeLayer = new RouteLayer(mapView);
                 mapView.addLayer(routeLayer);
 
@@ -61,32 +77,43 @@ public class RouteLayerTestActivity extends AppCompatActivity {
                 markLayer.setMarkIsClickListener(new MarkLayer.MarkIsClickListener() {
                     @Override
                     public void markIsClick(int num) {
+
                         PointF target = new PointF(marks.get(num).x, marks.get(num).y);
-                        List<Integer> routeList = MapUtils.getShortestDistanceBetweenTwoPoints
+                        routeList = MapUtils.getShortestDistanceBetweenTwoPoints
                                 (marks.get(39), target, nodes, nodesContract);
                         routeLayer.setNodeList(nodes);
                         routeLayer.setRouteList(routeList);
                         mapView.refresh();
+
+                        Timer timer = new Timer();
+                        timer.schedule(new MoveTask(), 0, 1000);
                     }
                 });
                 mapView.refresh();
+
             }
+
 
             @Override
             public void onMapLoadFail() {
+
             }
 
         });
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         getMenuInflater().inflate(R.menu.menu_route_layer_test, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         if (mapView.isMapLoadFinish()) {
             switch (item.getItemId()) {
                 case R.id.route_layer_tsp:
@@ -97,7 +124,7 @@ public class RouteLayerTestActivity extends AppCompatActivity {
                     list.add(marks.get(new Random().nextInt(10) + 20));
                     list.add(marks.get(new Random().nextInt(10) + 9));
                     List<Integer> routeList = MapUtils.getBestPathBetweenPoints(list, nodes,
-                            nodesContract);
+                                                                                nodesContract);
                     routeLayer.setNodeList(nodes);
                     routeLayer.setRouteList(routeList);
                     mapView.refresh();
@@ -107,5 +134,30 @@ public class RouteLayerTestActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    class MoveTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            RouteLayerTestActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (routeList == null) {
+                        throw new NullPointerException("Route list is null");
+                    }
+
+                    if (!routeList.isEmpty()) {
+                        PointF currentPosition = nodes.get(routeList.get(0));
+                        locationLayer.setCurrentPosition(currentPosition);
+                        mapView.refresh();
+                        routeList.remove(0);
+                    }
+                }
+            });
+        }
     }
 }
